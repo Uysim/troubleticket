eb330d73d05f1abbbbfb4f0f499509d83d83b2c3
Uysim <uysimty@gmail.com>
2016-12-27 20:04:56 +0700

    scaffold trouble

---
+++ db/migrate/20161227130250_create_troubles.rb
@@ -0,0 +1,11 @@
+class CreateTroubles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :troubles do |t|
+      t.references :client, foreign_key: true
+      t.references :user, foreign_key: true
+      t.text :detail
+
+      t.timestamps
+    end
+  end
+end
