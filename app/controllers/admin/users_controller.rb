e37d27c0b7c4dc649fa53033b60e1353308967f1
Uysim <uysimty@gmail.com>
2016-12-29 21:10:43 +0700

    pundit base

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 module Admin
-  class UsersController < ApplicationController
+  class UsersController < AdminBaseController
     before_action :find_user, only: [:show, :edit, :update, :destroy]
 
     def index
