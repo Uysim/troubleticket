d9d6b79d1641ab3cf0ace668c7052bbcdfaf7a9f
chileap <chileapchhin@gmail.com>
2016-12-30 22:03:12 +0700

    remove some validation of client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,13 +3,11 @@ class Client < ApplicationRecord
 
   validates :name,      presence: true
   validates :id_number, presence: true, uniqueness: true
-  validates :phone,     presence: true
   validates :email,     presence: true
-  validates :address,   presence: true
 
   scope :id_number_like, -> (id_number) { where('clients.id_number iLike :id_number', id_number: "#{id_number}") }
   scope :phone_like, -> (phone) { where('clients.phone iLike :phone', phone: "#{phone}") }
   scope :email_like, -> (email) { where('clients.email iLike :email', email: "#{email}") }
   scope :name_like, ->(name){ where('clients.name iLike :name', name: "%#{name}%") }
-  
+
 end
