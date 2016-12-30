828c5fd6456a202d7baabea9647b95d6926ae833
chileap <chileapchhin@gmail.com>
2016-12-30 12:11:34 +0700

    add uniqueness to client id number

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
   has_many :troubles, dependent: :destroy
 
   validates :name,      presence: true
-  validates :id_number, presence: true
+  validates :id_number, presence: true, uniqueness: true
   validates :phone,     presence: true
   validates :email,     presence: true
   validates :address,   presence: true
