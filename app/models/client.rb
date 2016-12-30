731a6e2959c1a2323b7c314bfbc02f8c350ac71a
chileap <chileapchhin@gmail.com>
2016-12-30 10:00:02 +0700

    validate clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,11 @@
 class Client < ApplicationRecord
   has_many :troubles, dependent: :destroy
+
+  validates :name,      presence: true
+  validates :id_number, presence: true
+  validates :phone,     presence: true
+  validates :email,     presence: true
+  validates :address,   presence: true
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
