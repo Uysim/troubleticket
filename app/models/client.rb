46243beec881dbd10d6d7d92ffd7bd4860b07e2e
Uysim <uysimty@gmail.com>
2016-12-31 10:58:55 +0700

    auto generate client number

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,9 +7,16 @@ class Client < ApplicationRecord
   validates :email,     presence: true
   validates :address,   presence: true
 
+  after_initialize :generate_id_number, if: :new_record?
+
   scope :id_number_like, -> (id_number) { where('clients.id_number iLike :id_number', id_number: "#{id_number}") }
   scope :phone_like, -> (phone) { where('clients.phone iLike :phone', phone: "#{phone}") }
   scope :email_like, -> (email) { where('clients.email iLike :email', email: "#{email}") }
   scope :name_like, ->(name){ where('clients.name iLike :name', name: "%#{name}%") }
-  
+
+  private
+
+  def generate_id_number
+    self.id_number = "client-#{Client.count+1}"
+  end
 end
